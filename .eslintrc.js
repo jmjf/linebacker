421d547d279f7cd46c6ffae86c6e85a1cefd3e9e
jmjf <jamee.mikell@gmail.com>
2022-04-09 21:39:34 +0000

    3.1.1.1 - changes trying to get @paths to work (failed)

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+   "env": {
+      "browser": false,
+      "amd": false,
+      "node": true 
+   },
+   root: true,
+   parser: "@typescript-eslint/parser",
+   plugins: [
+      "@typescript-eslint",
+   ],
+   extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
+   ],
+   rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+   }
+}
