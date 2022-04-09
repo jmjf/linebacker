1b9d9bdfed9f0171f45a87810ca8a4610b16d57c
jmjf <jamee.mikell@gmail.com>
2022-04-09 01:01:00 +0000

    2.1.0 - create Entity and Result

---
+++ src/ddd/Entity.ts
@@ -0,0 +1,66 @@
+import uuid from 'uuid';
+
+/**
+ * Type guard for the Entity class
+ * 
+ * @remarks
+ * See https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+ * 
+ * The `v is Entity<any>` type predicate return type makes `isEntity` act as a type guard.
+ * 
+ * After calling isEntity(), TypeScript narrows the type of the passed parameter based on
+ * the return value. In the true branch, it is an Entity and can be referecned as one.
+ * 
+ * In the TS docs example, the type is either a Fish or a Bird, so if it isn't a Fish, TS
+ * knows it must be a Bird.
+ * 
+ * @param {any} v - value to be tested
+ * @returns {boolean}
+ */
+const isEntity = (v: any): v is Entity<any> => {
+  return v instanceof Entity;
+};
+
+/**
+ * Abstract class representing a domain driven design entity.
+ * 
+ * @typeParam T - type of the Entity's properties object (interface)
+ */
+export abstract class Entity<T> {
+  protected readonly _id: string;
+  public readonly props: T;
+
+  /**
+   * 
+   * @param {T} props - an object shaped like the entity's properties
+   * @param {string} id - optional, must be a valid uuid (not validated)
+   * 
+   * @remarks if id isn't provided, will generate a uuid
+   */
+  constructor (props: T, id?: string) {
+    this._id = id ? id : uuid.v4();
+    this.props = props;
+  }
+
+  /**
+   * 
+   * @param {Entity<T>} object - optional, object to test for equality to this
+   * @returns {boolean}
+   */
+  public equals (object?: Entity<T>) : boolean {
+
+    if (object == null || object == undefined) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!isEntity(object)) {
+      return false;
+    }
+
+    return this._id === object._id;
+  }
+}
---
+++ src/ddd/Result.ts
@@ -0,0 +1,144 @@
+/**
+ * A class that represents an operation's outcome
+ * 
+ * @typeparam T - Type of the Result's value or error object (interface)
+ * 
+ * @remarks
+ * Also provides factory functions to support working with Results
+ */
+export class Result<T> {
+  public isSuccess: boolean;
+  public isFailure: boolean
+  public error: T | string;
+  private _value: T;
+
+  /**
+   * 
+   * @param {boolean} isSuccess - identifies if the operation was successful
+   * @param {T | string} error - optional, an error message or object returned by a failed operation
+   * @param {T} value - optional, a value returned by an operation (successful or failed)
+   */
+  public constructor (isSuccess: boolean, error?: T | string, value?: T) {
+    if (isSuccess && error) {
+      throw new Error("InvalidOperation: A result cannot be successful and contain an error");
+    }
+    if (!isSuccess && !error) {
+      throw new Error("InvalidOperation: A failing result needs to contain an error message");
+    }
+
+    this.isSuccess = isSuccess;
+    this.isFailure = !isSuccess;
+    this.error = error;
+    this._value = value;
+    
+    // do not allow any changes
+    Object.freeze(this);
+  }
+
+  /**
+   * 
+   * @returns {T} - the value of a successful operation
+   */
+  public getValue () : T {
+    if (!this.isSuccess) {
+      console.log(this.error,);
+      throw new Error("Can't get the value of an error result. Use 'errorValue' instead.")
+    } 
+
+    return this._value;
+  }
+
+  /**
+   * 
+   * @returns {T} - the value of an unsuccessful operation
+   */
+  public errorValue (): T {
+    return this.error as T;
+  }
+
+  /**
+   * @remarks
+   * Generally, prefer using `Result.ok()` over the bare constructor
+   * 
+   * @typeparam U - Type of the Result's value object (interface)
+   * 
+   * @param {U} value - optional, a value to use for the result
+   * @returns {Result<U>} - a successful result
+   */
+  public static ok<U> (value?: U) : Result<U> {
+    return new Result<U>(true, null, value);
+  }
+
+  /**
+   * @remarks
+   * Generally, prefer using `Result.fail()` over the bare constructor
+   * 
+   * @typeparam U - Type of the Result's error object (interface) [check this]
+   * 
+   * @param {U} error - a string representing the error message
+   * @returns {Result<U>} - a failure result
+   */
+  public static fail<U> (error: string): Result<U> {
+    return new Result<U>(false, error);
+  }
+
+  /**
+   * 
+   * @param {Result<any>[]} results - a list of results to summarize
+   * @returns {Result<any>} - the net outcome of the list of results
+   * 
+   * @remarks
+   * If any result fails, `Result.combine()` returns that result
+   * If no result fails, `Result.combine()` returns Result.ok()
+   */
+  public static combine (results: Result<any>[]) : Result<any> {
+    for (let result of results) {
+      if (result.isFailure) return result;
+    }
+    return Result.ok();
+  }
+}
+
+// I'm keeping these for now, but need to figure out what they're really doing.
+
+export type Either<L, A> = Left<L, A> | Right<L, A>;
+
+export class Left<L, A> {
+  readonly value: L;
+
+  constructor(value: L) {
+    this.value = value;
+  }
+
+  isLeft(): this is Left<L, A> {
+    return true;
+  }
+
+  isRight(): this is Right<L, A> {
+    return false;
+  }
+}
+
+export class Right<L, A> {
+  readonly value: A;
+
+  constructor(value: A) {
+    this.value = value;
+  }
+
+  isLeft(): this is Left<L, A> {
+    return false;
+  }
+
+  isRight(): this is Right<L, A> {
+    return true;
+  }
+}
+
+export const left = <L, A>(l: L): Either<L, A> => {
+  return new Left(l);
+};
+
+export const right = <L, A>(a: A): Either<L, A> => {
+  return new Right<L, A>(a);
+};
