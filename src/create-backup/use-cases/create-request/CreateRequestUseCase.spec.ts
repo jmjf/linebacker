765302b76e8f2bb38eb3bd342358d33c44d4df7a
jmjf <jamee.mikell@gmail.com>
2022-05-02 19:22:23 +0000

    3.1.2.3 - use repo factory in create request tests

--- src/create-backup/use-cases/create-request/CreateRequestUseCase.spec.ts
+++ src/create-backup/use-cases/create-request/CreateRequestUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { CreateRequestUseCase } from './CreateRequestUseCase';
 import { CreateRequestDTO } from './CreateRequestDTO';
 import { IBackupRequestRepo } from '../../adapter/BackupRequestRepo';
+import { backupRequestRepoFactory } from '../../test-utils/backupRequestRepoFactory';
 
 /**
  * See notes on testing with TypeORM in devnotes/3.1.1-RequestBackupUseCase
@@ -19,8 +20,8 @@ describe('Create Request Use Case', () => {
 
    test('when executed with good data, it returns a saved backupRequest', async () => {
       // Arrange
-      const repo = {} as IBackupRequestRepo;
-      repo.save = jest.fn().mockResolvedValue('resolved');
+      const repo = backupRequestRepoFactory();
+      const saveSpy = jest.spyOn(repo, 'save');
       const useCase = new CreateRequestUseCase(repo);
       const dto = { ...baseDto };
       
@@ -29,15 +30,15 @@ describe('Create Request Use Case', () => {
 
       // Assert
       expect(result.isRight()).toBe(true);
-      expect(repo.save).toHaveBeenCalledTimes(1);
+      expect(saveSpy).toHaveBeenCalledTimes(1);
       expect(result.value.getValue().backupJobId).toMatch(baseDto.backupJobId);
       expect(result.value.getValue().backupRequestId).toBeTruthy();
    });
 
    test('when executed with an invalid transport type, it returns the expected error', async() => {
       // Arrange
-      const repo = {} as IBackupRequestRepo;
-      repo.save = jest.fn().mockResolvedValue('resolved');
+      const repo = backupRequestRepoFactory();
+      const saveSpy = jest.spyOn(repo, 'save');
       const useCase = new CreateRequestUseCase(repo);
       const dto = { ...baseDto, transportType: 'BadTransport'};
       
@@ -47,13 +48,13 @@ describe('Create Request Use Case', () => {
       // Assert
       expect(result.isLeft()).toBe(true);
       expect(result.value.error).toContain('transportType is not one of');
-      expect(repo.save).toHaveBeenCalledTimes(0);
+      expect(saveSpy).toHaveBeenCalledTimes(0);
    });
 
    test('when executed with an undefined required value, it returns the expected error', async() => {
       // Arrange
-      const repo = {} as IBackupRequestRepo;
-      repo.save = jest.fn().mockResolvedValue('resolved');
+      const repo = backupRequestRepoFactory();
+      const saveSpy = jest.spyOn(repo, 'save');
       const useCase = new CreateRequestUseCase(repo);
       // TypeScript won't let me delete dto.createOnStartFlag, so build a dto without it
       const dto = { 
@@ -70,13 +71,13 @@ describe('Create Request Use Case', () => {
       // Assert
       expect(result.isLeft()).toBe(true);
       expect(result.value.error).toContain('getOnStartFlag is null or undefined');
-      expect(repo.save).toHaveBeenCalledTimes(0);
+      expect(saveSpy).toHaveBeenCalledTimes(0);
    });
 
    test('when executed with an invalid dataDate, it returns the expected error', async() => {
       // Arrange
-      const repo = {} as IBackupRequestRepo;
-      repo.save = jest.fn().mockResolvedValue('resolved');
+      const repo = backupRequestRepoFactory();
+      const saveSpy = jest.spyOn(repo, 'save');
       const useCase = new CreateRequestUseCase(repo);
       const dto = { ...baseDto, dataDate: 'invalid date'};
       
@@ -86,6 +87,6 @@ describe('Create Request Use Case', () => {
       // Assert
       expect(result.isLeft()).toBe(true);
       expect(result.value.error).toContain('dataDate is not a valid date');
-      expect(repo.save).toHaveBeenCalledTimes(0);
+      expect(saveSpy).toHaveBeenCalledTimes(0);
    });
 });
