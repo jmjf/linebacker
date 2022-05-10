4b0597d50602e590bcd0766e1de067dc0ba49a55
jmjf <jamee.mikell@gmail.com>
2022-05-10 23:47:06 +0000

    3.1.7.1 - REFACTOR: Rename types on Either to be clearer

--- src/common/domain/Either.ts
+++ src/common/domain/Either.ts
@@ -1,8 +1,8 @@
 /**
  * A union type that represents a value with two possibilities.
  * 
- * @typeparam `L` type for the value of the Left side of Either
- * @typeparam `A` type for the value of the right side of Either
+ * @typeparam `LeftType` type for the value of the Left side of Either
+ * @typeparam `RightType` type for the value of the right side of Either
  * 
  * @remarks Either is derived from functional languages like Haskell. It's commonly used to represent a value
  * that may be either correct or an error. By convention, Left is the error and Right is the correct value.
@@ -10,23 +10,23 @@
  * 
  * To use, construct an Either by calling the left() or right() factory functions provided below.
  */
- export type Either<L, A> = Left<L, A> | Right<L, A>;
+ export type Either<LeftType, RightType> = Left<LeftType, RightType> | Right<LeftType, RightType>;
 
  /**
   * A class that represents the left side value of an Either
   */
- export class Left<L, A> {
-    readonly value: L;
+ export class Left<LeftType, RightType> {
+    readonly value: LeftType;
  
-    constructor(value: L) {
+    constructor(value: LeftType) {
        this.value = value;
     }
  
-    isLeft(): this is Left<L, A> {
+    isLeft(): this is Left<LeftType, RightType> {
        return true;
     }
  
-    isRight(): this is Right<L, A> {
+    isRight(): this is Right<LeftType, RightType> {
        return false;
     }
  }
@@ -34,18 +34,18 @@
  /**
   * A class that represents the left side value of an Either
   */
- export class Right<L, A> {
-    readonly value: A;
+ export class Right<LeftType, RightType> {
+    readonly value: RightType;
  
-    constructor(value: A) {
+    constructor(value: RightType) {
        this.value = value;
     }
  
-    isLeft(): this is Left<L, A> {
+    isLeft(): this is Left<LeftType, RightType> {
        return false;
     }
  
-    isRight(): this is Right<L, A> {
+    isRight(): this is Right<LeftType, RightType> {
        return true;
     }
  }
@@ -55,9 +55,9 @@
   * 
   * @param l the value of the Left returned
   * 
-  * @typeParam `L` the type of l (the left)
+  * @typeParam `LeftType` the type of the left
   * 
-  * @returns `Either<L, A>` that is a Left
+  * @returns `Either<LeftType, RightType>` that is a Left
   * 
   * @example
   * ```
@@ -66,21 +66,21 @@
   * }
   * ```
   */
- export const left = <L, A>(l: L): Either<L, A> => {
+ export const left = <LeftType, RightType>(l: LeftType): Either<LeftType, RightType> => {
     return new Left(l);
  };
  
  /**
   * A factory function that returns a Right side Either.
   * 
-  * @param a the value of the Right returned
+  * @param r the value of the Right returned
   * 
-  * @typeParam `A` the type of a (the right)
+  * @typeParam `RightType` the type of a the right
   * 
-  * @returns `Either<L, A>` that is a Right
+  * @returns `Either<LeftType, RightType>` that is a Right
   * 
   * @example `return right(Result.ok(<void>));`
   */
- export const right = <L, A>(a: A): Either<L, A> => {
-    return new Right<L, A>(a);
+ export const right = <LeftType, RightType>(r: RightType): Either<LeftType, RightType> => {
+    return new Right<LeftType, RightType>(r);
  };
