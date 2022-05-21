5b87100bbe44581bc0e36c15c839eae32494a7bc
jmjf <jamee.mikell@gmail.com>
2022-05-21 20:13:23 +0000

    4.1.3 - ADD: route for square/:x

--- src/zz-learn-fastify/routes.ts
+++ src/zz-learn-fastify/routes.ts
@@ -2,8 +2,22 @@ import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } f
 
 export async function routes(server: FastifyInstance, options: FastifyPluginOptions) {
    server.log.info(`called routes with options ${JSON.stringify(options)}`);
+
    server.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
       server.log.info('hello world');
       return { hello: 'world' };
+   });
+
+   server.get('/square/:x', async (request: FastifyRequest, reply: FastifyReply) => {
+      // params is type unknown, which makes sense, so declare local type and use it
+      type ParamsType = {
+         x: number
+      };
+
+      const x = (<ParamsType>request.params).x;
+
+      server.log.info(`square called with ${x}`);
+
+      return { param: x, square: x*x };
    });
 }
