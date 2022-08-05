476de6e367bfbd439cb1290ddac2acaf19481fe6
jmjf <jamee.mikell@gmail.com>
2022-08-05 20:11:46 +0000

    6.3.4 - CHORE: move Fastify controller implementation to .../adapter/impl where it belongs

---
+++ src/backup-request/adapter/impl/FastifyCreateBackupRequestController.spec.ts
@@ -0,0 +1,139 @@
+import * as uuid from 'uuid';
+
+import { buildApp } from '../../../app';
+
+import { ICreateBackupRequestBody } from './FastifyCreateBackupRequestController';
+
+import {
+	MockPrismaContext,
+	PrismaContext,
+	createMockPrismaContext,
+} from '../../../common/infrastructure/database/prismaContext';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { RequestStatusTypeValues } from '../../domain/RequestStatusType';
+
+describe('CreateBackupRequestFastifyController', () => {
+	let mockPrismaCtx: MockPrismaContext;
+	let prismaCtx: PrismaContext;
+
+	beforeEach(() => {
+		mockPrismaCtx = createMockPrismaContext();
+		prismaCtx = mockPrismaCtx as unknown as PrismaContext;
+	});
+
+	const basePayload = {
+		apiVersion: '2022-05-22',
+		backupJobId: 'job-id',
+		dataDate: '2022-05-30',
+		backupDataLocation: 'data-location',
+	} as ICreateBackupRequestBody;
+
+	test('when apiVersion is invalid, it returns 400 and an error', async () => {
+		// Arrange
+		const app = buildApp(prismaCtx);
+
+		// Act
+		const response = await app.inject({
+			method: 'POST',
+			url: '/backup-request',
+			payload: {
+				...basePayload,
+				apiVersion: 'invalid',
+			},
+		});
+
+		// Assert
+		expect(response.statusCode).toBe(400);
+		// convert body to an object we can use -- may throw an error if body isn't JSON
+		const body = JSON.parse(response.body);
+		expect(body.code).toBe('InvalidApiVersion');
+	});
+
+	test('when the use case gets a database error, the controller returns 500 and a low-leak error', async () => {
+		// Arrange
+		// simulate a database error
+		const prismaCode = 'P1012';
+		mockPrismaCtx.prisma.backupRequest.upsert.mockRejectedValue(
+			new PrismaClientKnownRequestError(
+				'Key is already defined',
+				prismaCode,
+				'2'
+			)
+		);
+		const app = buildApp(prismaCtx);
+
+		// Act
+		const response = await app.inject({
+			method: 'POST',
+			url: '/backup-request',
+			payload: {
+				...basePayload,
+			},
+		});
+
+		// Assert
+		expect(response.statusCode).toBe(500);
+		// convert payload to an object we can use -- may throw an error if payload isn't JSON
+		const payload = JSON.parse(response.payload);
+		expect(payload.code).toBe('Database');
+		expect(payload.message).toBe(prismaCode.slice(1)); // ensure message is clean
+	});
+
+	test('when the use case returns a PropsError, the controller returns 400 and an error', async () => {
+		// Arrange
+		const app = buildApp(prismaCtx);
+
+		// Act
+		const response = await app.inject({
+			method: 'POST',
+			url: '/backup-request',
+			payload: {
+				...basePayload,
+				dataDate: '', // easy error to force
+			},
+		});
+
+		// Assert
+		expect(response.statusCode).toBe(400);
+		// convert payload to an object we can use -- may throw an error if payload isn't JSON
+		const payload = JSON.parse(response.payload);
+		expect(payload.code).toBe('BadData');
+		expect(payload.message).toMatch('dataDate');
+	});
+
+	test('when request data is good, the controller returns Accepted and a result payload', async () => {
+		// Arrange
+		const app = buildApp(prismaCtx);
+
+		// Act
+		const startTime = new Date();
+		const response = await app.inject({
+			method: 'POST',
+			url: '/backup-request',
+			payload: {
+				...basePayload,
+			},
+		});
+		const endTime = new Date();
+
+		// Assert
+		expect(response.statusCode).toBe(202);
+		// convert payload to an object we can use -- may throw an error if payload isn't JSON
+		const payload = JSON.parse(response.payload);
+		const receivedTimestamp = new Date(payload.receivedTimestamp);
+
+		expect(uuid.validate(payload.backupRequestId)).toBe(true);
+		expect(payload.statusTypeCode).toBe(RequestStatusTypeValues.Received);
+		expect(payload.backupJobId).toBe(basePayload.backupJobId);
+		expect(payload.preparedDataPathName).toBe(
+			basePayload.backupDataLocation
+		);
+		expect(payload.dataDate).toBe(basePayload.dataDate);
+		expect(receivedTimestamp.valueOf()).toBeGreaterThanOrEqual(
+			startTime.valueOf()
+		);
+		expect(receivedTimestamp.valueOf()).toBeLessThanOrEqual(
+			endTime.valueOf()
+		);
+	});
+});
--- src/backup-request/infrastructure/http/fastifyRoutes.ts
+++ src/backup-request/infrastructure/http/fastifyRoutes.ts
@@ -1,24 +1,40 @@
 import { PrismaContext } from '../../../common/infrastructure/database/prismaContext';
 import { CreateBackupRequestUseCase } from '../../use-cases/create-backup-request/CreateBackupRequestUseCase';
-import { FastifyCreateBackupRequestController } from '../../adapter/FastifyCreateBackupRequestController';
+import { FastifyCreateBackupRequestController } from '../../adapter/impl/FastifyCreateBackupRequestController';
 import { PrismaBackupRequestRepo } from '../../adapter/impl/PrismaBackupRequestRepo';
-import { RealFastifyReply, RealFastifyRequest, RealFastifyInstance } from '../../../common/adapter/FastifyController';
+import {
+	RealFastifyReply,
+	RealFastifyRequest,
+	RealFastifyInstance,
+} from '../../../common/adapter/FastifyController';
 
-export function addBackupRequestRoutes(app: RealFastifyInstance, prismaCtx: PrismaContext) {
-   const prismaBackupRequestRepo = new PrismaBackupRequestRepo(prismaCtx);
-   const createBackupRequestUseCase = new CreateBackupRequestUseCase(prismaBackupRequestRepo);
-   const fastifyCreateBackupRequestController = new FastifyCreateBackupRequestController(createBackupRequestUseCase);
+export function addBackupRequestRoutes(
+	app: RealFastifyInstance,
+	prismaCtx: PrismaContext
+) {
+	const prismaBackupRequestRepo = new PrismaBackupRequestRepo(prismaCtx);
+	const createBackupRequestUseCase = new CreateBackupRequestUseCase(
+		prismaBackupRequestRepo
+	);
+	const fastifyCreateBackupRequestController =
+		new FastifyCreateBackupRequestController(createBackupRequestUseCase);
 
-   app.post('/backup-request', async function (request: RealFastifyRequest, reply: RealFastifyReply) {
-      const result = await fastifyCreateBackupRequestController.execute(request, reply);
-      // HTTP status > 399 is an error
-      if (reply.statusCode > 399) {
-         app.log.error(result);
+	app.post(
+		'/backup-request',
+		async function (request: RealFastifyRequest, reply: RealFastifyReply) {
+			const result = await fastifyCreateBackupRequestController.execute(
+				request,
+				reply
+			);
+			// HTTP status > 399 is an error
+			if (reply.statusCode > 399) {
+				app.log.error(result);
 
-         if (result.name === 'DatabaseError') {
-            result.cleanMessage();
-         };
-      }
-      reply.send(result);
-   });
+				if (result.name === 'DatabaseError') {
+					result.cleanMessage();
+				}
+			}
+			reply.send(result);
+		}
+	);
 }
