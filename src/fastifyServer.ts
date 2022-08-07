8d1624e71bda4c54106313f78062ecdbb19e411a
jmjf <jamee.mikell@gmail.com>
2022-08-07 14:23:26 +0000

    7.1.3 - FIX: test and settle issues with module resolution

--- src/fastifyServer.ts
+++ src/fastifyServer.ts
@@ -3,50 +3,52 @@ import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 
 import { PrismaContext } from './common/infrastructure/database/prismaContext';
-import { buildApp } from '../src/fastifyApp';
+import { buildApp } from './fastifyApp';
 
-console.log('linebacker | getting environment');
-if (!process.env.APP_ENV) {
-	console.error(`main | APP_ENV is falsey`);
-	process.exit(1);
-}
+const startServer = async () => {
+	const logContext = 'linebacker | startServer';
 
-console.log(`linebacker | APP_ENV ${process.env.APP_ENV}`);
-dotenv.config({ path: `./env/${process.env.APP_ENV}.env` });
+	console.log(`${logContext} | getting environment`);
+	if (!process.env.APP_ENV) {
+		console.error(`main | APP_ENV is falsey`);
+		process.exit(1);
+	}
 
-if (!process.env.API_PORT || process.env.API_PORT.length === 0) {
-	console.log('linebacker | API_PORT is falsey or empty');
-	process.exit(1);
-}
-const apiPort = parseInt(process.env.API_PORT);
-console.log(`linebacker | apiPort ${apiPort}`);
+	console.log(`${logContext} | APP_ENV ${process.env.APP_ENV}`);
+	dotenv.config({ path: `./env/${process.env.APP_ENV}.env` });
 
-console.log('linebacker | creating Prisma client');
-const prismaCtx: PrismaContext = {
-	prisma: new PrismaClient(),
-};
-console.log('linebacker | connecting Prisma client');
-await prismaCtx.prisma.$connect();
+	if (!process.env.API_PORT || process.env.API_PORT.length === 0) {
+		console.log(`${logContext} | API_PORT is falsey or empty`);
+		process.exit(1);
+	}
+	const apiPort = parseInt(process.env.API_PORT);
+	console.log(`${logContext} | apiPort ${apiPort}`);
 
-console.log('linebacker | building server');
-const server = buildApp(prismaCtx, {
-	logger: {
-		level: 'debug',
-		transport: {
-			target: 'pino-pretty',
+	console.log(`${logContext} | creating Prisma client`);
+	const prismaCtx: PrismaContext = {
+		prisma: new PrismaClient(),
+	};
+	console.log(`${logContext} | connecting Prisma client`);
+	await prismaCtx.prisma.$connect();
+
+	console.log(`${logContext} | building server`);
+	const server = buildApp(prismaCtx, {
+		logger: {
+			level: 'debug',
+			transport: {
+				target: 'pino-pretty',
+			},
 		},
-	},
-});
+	});
 
-const start = async () => {
-	console.log('linebacker | starting server');
+	console.log(`${logContext} | starting server`);
 	try {
 		const address = await server.listen({ port: apiPort });
-		console.log(`linebacker | listening on ${address}`);
+		console.log(`${logContext} | listening on ${address}`);
 	} catch (err) {
 		server.log.error(err);
 		process.exit(1);
 	}
 };
 
-start();
+startServer();
