c5214a763b059460a6509a0a34cac4aacac0b1e5
jmjf <jamee.mikell@gmail.com>
2022-11-06 13:35:41 +0000

    FEAT: add pm2healthz to zpages

--- src/test-helpers/pm2Info.ts
+++ src/test-helpers/pm2Info.ts
@@ -1,15 +1,78 @@
 import pm2 from 'pm2';
+import util from 'util';
 
-pm2.connect((err) => {
-	if (err) {
-		console.log('err', err);
-		process.exit(2);
-	}
+// eslint-disable-next-line @typescript-eslint/ban-types
+function promisifyPm2(f: Function, theThis: unknown) {
+	return (...args: unknown[]) => {
+		// return a wrapper-function (*)
+		return new Promise((resolve, reject) => {
+			function callback(err: Error, result: unknown) {
+				// our custom callback for f (**)
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			}
 
-	pm2.list((err, list) => {
-		console.log('err', err);
-		console.log('list', list);
+			args.push(callback); // append our custom callback to the end of f arguments
 
-		pm2.disconnect();
-	});
-});
+			f.call(theThis, ...args); // call the original function
+		});
+	};
+}
+
+const pm2Connect = promisifyPm2(pm2.connect, pm2);
+const pm2List = promisifyPm2(pm2.list, pm2);
+
+async function main() {
+	await pm2Connect();
+
+	const list = await pm2List();
+
+	pm2.disconnect();
+
+	console.log('list', list);
+}
+
+main();
+// pm2.list((err, list) => {
+// 	console.log('err', err);
+// 	console.log(
+// 		'list',
+// 		list.map((item) => {
+// 			const { name, pm_id, pid, monit, pm2_env } = item;
+// 			const {
+// 				instances,
+// 				exec_mode,
+// 				status,
+// 				created_at,
+// 				pm_uptime,
+// 				restart_time,
+// 				version,
+// 				node_version,
+// 				axm_monitor,
+// 			} = pm2_env as any;
+// 			const valueOrNa = (value: unknown) => value || 'not available';
+// 			return {
+// 				name,
+// 				pm2ProcessId: pm_id,
+// 				pid,
+// 				memory: valueOrNa(monit?.memory),
+// 				cpu: valueOrNa(monit?.cpu),
+// 				instances: valueOrNa(instances),
+// 				execMode: valueOrNa(exec_mode),
+// 				status: valueOrNa(status),
+// 				startTime: valueOrNa(created_at),
+// 				uptime: valueOrNa(pm_uptime),
+// 				restartCount: valueOrNa(restart_time),
+// 				version: valueOrNa(version),
+// 				node_version: valueOrNa(node_version),
+// 				axm_monitor: valueOrNa(axm_monitor),
+// 			};
+// 		})
+// 	);
+
+// 	pm2.disconnect();
+// 	});
+// });
--- src/zpages/infrastructure/expressRoutes.ts
+++ src/zpages/infrastructure/expressRoutes.ts
@@ -1,8 +1,34 @@
-import { Request, Response, Router } from 'express';
 import path from 'path';
+import pm2 from 'pm2';
+import { Request, Response, Router } from 'express';
 import { getRequestStats } from '.';
 
 const moduleName = path.basename(module.filename);
+
+// To promisify pm2 functions, we need a custom promisifiy function because pm2 needs 'this'
+// eslint-disable-next-line @typescript-eslint/ban-types
+function promisifyPm2(f: Function) {
+	return (...args: unknown[]) => {
+		// return a wrapper-function (*)
+		return new Promise((resolve, reject) => {
+			function callback(err: Error, result: unknown) {
+				// our custom callback for f (**)
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			}
+
+			args.push(callback); // append our custom callback to the end of f arguments
+
+			f.call(pm2, ...args); // call the original function
+		});
+	};
+}
+
+const pm2Connect = promisifyPm2(pm2.connect);
+const pm2List = promisifyPm2(pm2.list);
 
 export interface ZpageDependencyCheck {
 	depName: string;
@@ -45,6 +71,55 @@ export function getZpagesRouter(dependencies: ZpageDependencies) {
 			requestStats: getRequestStats(),
 		};
 		response.status(200).send(body);
+	});
+
+	router.get('/pm2healthz', async (request: Request, response: Response) => {
+		let errorFrom = '';
+		try {
+			errorFrom = 'connect';
+			await pm2Connect();
+
+			errorFrom = 'list';
+			const pm2Processes = (await pm2List()) as pm2.ProcessDescription[];
+
+			pm2.disconnect(); // does not take a callback
+
+			response.status(200).send(
+				pm2Processes.map((item) => {
+					const { name, pm_id, pid, monit, pm2_env } = item;
+					const {
+						instances,
+						exec_mode,
+						status,
+						created_at,
+						pm_uptime,
+						restart_time,
+						version,
+						node_version,
+						axm_monitor,
+					} = pm2_env as any;
+					const valueOrNa = (value: unknown) => value || 'not available';
+					return {
+						name,
+						pm2ProcessId: pm_id,
+						pid,
+						memory: valueOrNa(monit?.memory),
+						cpu: valueOrNa(monit?.cpu),
+						instances: valueOrNa(instances),
+						execMode: valueOrNa(exec_mode),
+						status: valueOrNa(status),
+						startTime: valueOrNa(created_at),
+						uptime: valueOrNa(pm_uptime),
+						restartCount: valueOrNa(restart_time),
+						version: valueOrNa(version),
+						node_version: valueOrNa(node_version),
+						axm_monitor: valueOrNa(axm_monitor),
+					};
+				})
+			);
+		} catch (e) {
+			response.status(500).send({ message: `pm2 ${errorFrom} error`, error: e });
+		}
 	});
 
 	return router;
